36e3c6837bb338faf61fffd6888bdca2354a1bbc
Christian <zerov83@gmail.com>
2016-03-09 07:17:04 +0100

    Skindesigner "DetailView" in development, pushed missing files.

---
+++ playlist.cpp
@@ -0,0 +1,11 @@
+#include "playlist.h"
+
+namespace plexclient
+{
+
+Playlist::Playlist(Poco::XML::Node* pNode, MediaContainer* parent)
+{
+}
+
+}
+
---
+++ playlist.h
@@ -0,0 +1,19 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
+#include "XmlObject.h" // Base class: plexclient::XmlObject
+#include "MediaContainer.h"
+
+namespace plexclient
+{
+
+class Playlist : private plexclient::XmlObject
+{
+public:
+	Playlist(Poco::XML::Node* pNode, MediaContainer* parent);
+
+};
+
+}
+
+#endif // PLAYLIST_H
